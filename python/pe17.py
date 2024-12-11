a1c92d58814feef872d44fa56f1590cec364a44b
Giles Ellis <gilesellis@gmail.com>
2024-12-11 09:39:42 +0000

    pe17: total string length of numbers written as words.

--- python/pe17.py
+++ python/pe17.py
@@ -12,9 +12,53 @@ import time
 import math
 
 start_time = time.time()
+MAX=1000
+RANGE=MAX
+if MAX<1000:
+    RANGE += 1
+
+digits = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+teens = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+digitsSize = []
+for dig in digits:
+    digitsSize.append(len(dig))
+teensSize = []
+for teen in teens:
+    teensSize.append(len(teen))
+tensSize = []
+for ten in tens:
+    tensSize.append(len(ten))
+
+numchars = 0
+for i in range(1, RANGE):
+    # print(f"i {i}")
+    hundreds = math.floor(i / 100)
+    num = i % 100
+    if hundreds > 0:
+        numchars += digitsSize[hundreds] + 7     # "hundred"
+        # print(f"HUNDREDS: i {i}, hundeds {hundreds} numchars {numchars}")
+        if num > 0:
+            numchars += 3   # "and"
+            # print(f"AND: i {i}, numchars {numchars}")
+
+    if num > 10 and num < 20:
+        numchars += teensSize[num - 10]
+        # print(f"TEENS: i {i}, numchars {numchars}")
+    else:
+        tens = math.floor(num / 10)
+        numchars += tensSize[tens]
+        # print(f"TENS: i {i}, num {tens} numchars {numchars}")
+        num = num % 10
+        numchars += digitsSize[num]
+        # print(f"DIGITS: i {i}, num {num} numchars {numchars}")
+
+if MAX == 1000:
+    numchars += len("onethousand")
 
 
-print(f"total {total}")
+print(f"numchars {numchars}")
 
 print(f"--- Number of seconds to solve {time.time() - start_time}")
 
