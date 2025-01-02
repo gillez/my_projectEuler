bc812ed7d67cb4a775ce8b68696854b0e6df54fb
Giles Ellis <gilesellis@gmail.com>
2025-01-02 17:48:34 +0000

    pe19 - num sundays on 1st of the month

--- python/pe19.py
+++ python/pe19.py
@@ -15,7 +15,36 @@ import math
 
 start_time = time.time()
 
-print(f"num sundays {numsundays}")
+days_not = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+days_leap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def daysForYear(year):
+    if (year % 4 == 0) and (year % 100 != 0 or year % 400 == 0):
+        return days_leap
+    else:
+        return days_not
+
+year = 1900
+days = 1
+sundays = 0
+
+# We know 1 Jan 1900 is Monday, what is 1 Jan 1901,
+# which is where we want to start counting?
+months = daysForYear(year)
+for month in months:
+    days += month
+year += 1
+
+while (year != 2001):
+    months = daysForYear(year)
+    for month in months:
+        days %= 7
+        if (days == 0):
+            sundays += 1
+        days += month
+    year += 1
+
+print(f"num sundays {sundays}")
 
 print(f"--- Number of seconds to solve {time.time() - start_time}")
 
