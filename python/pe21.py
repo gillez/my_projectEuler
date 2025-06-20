fdd64c182efc6515c440299ba159daf005b02973
Giles Ellis <gilesellis@gmail.com>
2025-06-20 10:34:08 +0100

    pe21: Faster Sieve solution

--- python/pe21.py
+++ python/pe21.py
@@ -14,32 +14,57 @@ import math
 
 start_time = time.time()
 
-answer = 0
-max = 10000
+max = 287
 
-sums = [0, 0]
+# Used for both solutions to find and add amicable pairs.
+def sumAmicable(sums):
+    answer = 0
+    for num in range(2, max):
+        if ((sums[num] < num) and (sums[int(sums[num])] == num)):
+            print(f"Amicable pair: {num} and {sums[num]}")
+            answer += num
+            answer += sums[num]
+    return answer
+
+# solution 1: loop to sqrt(target) and add factors
+
+sums = [1] * max
 
 def sumOfDividers(target):
     sum = 1
-    maxIter = math.floor(math.sqrt(target))
-    for x in range(2, maxIter, 1):
+    maxIter = int(math.sqrt(target))
+    for x in range(2, maxIter):
         if ((target % x) == 0):
             sum += x
             if ((target / x) != x):
                 sum += target / x
     return sum
 
-for num in range(2, 10000):
-    sums.append(sumOfDividers(num))
+for num in range(2, max):
+    sums[num] = sumOfDividers(num)
 
-for num in range(2, 10000):
-    if ((sums[num] < 10000) and (sums[num] > num) and (sums[int(sums[num])] == num)):
-        print(f"Amicable pair: {num} and {sums[num]}")
-        answer += num
-        answer += sums[num]
+answer = sumAmicable(sums)
 
-print(f"{answer}")
+print(f"solution 1 (calc factors): {answer}")
 
 print(f"--- Number of seconds to solve {time.time() - start_time}")
 
+# solution 1: sieve solution
+# Note, this is the fastest sieve solution given on the Udemy video, however, I have
+# realised that, although it given the correct answer in our case it's not quite
+# correct! If you reduce max to 285, the answers are not the same! This is
+# it doesn't calculate the last few numbers correctly, probably due to rounding when
+# calculatng int(math.sqrt(max)) and int(max/i)
+start_time = time.time()
 
+sums = [1] * max
+
+for i in range(2, int(math.sqrt(max))):
+    for j in range(i, int(max/i)):
+        sums[i*j] += i + j
+
+answer = sumAmicable(sums)
+
+print(f"solution 2 (sieve): {answer}")
+
+print(f"--- Number of seconds to solve {time.time() - start_time}")
